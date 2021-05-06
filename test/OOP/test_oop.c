606ea9109bb269be9fb37fcf64f7ff1061b41ac7
Chad Gibbons <46169822+gibbs212521@users.noreply.github.com>
2021-05-06 10:24:08 -0500

    minor refactoring of unit tests

--- test/OOP/test_oop.c
+++ test/OOP/test_oop.c
@@ -207,23 +207,3 @@ void test_4_test_membook_list_check()
     testResult(test_name, test_passed);
     
 }
-
-
-void runTest(bool condition, char class_method[], char test_case[], bool * ptest_passed)
-{
-    if(condition == true)
-        printf("\t %s \t %s \t PASSED\n",class_method, test_case);
-    else
-    {
-        printf("\t %s \t %s \t FAILED\n",class_method, test_case);
-        *ptest_passed = false;
-    }
-}
-
-void testResult(char test_name[], bool test_passed)
-{
-    if(test_passed==true)
-        printf("\t\t\t\t\t\t\t\t\t PASSED    %s\n", test_name);
-    else
-        printf("\t\t\t\t\t\t\t\t\t FAILED    %s\n", test_name);
-}
